e79a54b37a95da2d7e7001d65e127b96bfbcbb07
Yash Raj Gupta <yashraj.guptadev@gmail.com>
2024-09-15 21:51:17 +0530

    ui changes#12

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,106 +3,111 @@ import { useState } from 'react';
 import {  HiMenu, HiX } from 'react-icons/hi';
 import { IoIosArrowDown } from "react-icons/io";
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
 
     const [isCompanyOpen, setIsCompanyOpen] = useState(false);
     
-// const menuItems = [
-//     { name: 'home', link: '#' },
-//     { name: 'company', link: '#' },
-//     { name: 'projects', link: '#' }
-// ];
-return (
-        <nav className='app__navbar'>
-            <div className="app__navbar-logo">
-                <img src={'./logo.svg'} alt='logo' />
-            </div>
-            <ul className='app__navbar-links'>
-                {['home', 'company', 'projects'].map((item) => (
-                    <li className='app__links p-text' key={`link-${item}`}>
-                        {item === 'company' ? (
-                            < >
-                                <div className='flex flex-row gap-x-2  w-full items-center justify-center app__navbar-link-icon' onClick={() => setIsCompanyOpen(!isCompanyOpen)}>
-                                    {item}
-                                    <IoIosArrowDown />
-                                </div>
-                                {isCompanyOpen && (
-                                    <motion.div
-                                    whileInView={{  opacity: [0,1]}}
-                                    transation={{ duration: 1.5, ease: 'easeInOut', }}
-                                    className='app__navbar-dropdown'
-                                    >
-                                        <ul className="">
-                                        {['About Us', 'Carrers', 'Blogs', 'Contact Us', 'CSR', "FAQ'S"].map((item, index) => (
-                                        <li key={index}>
-                                            <a href="#">{item}</a>
-                                        </li>
-                                        ))}
-                                    </ul>
-                                    </motion.div>
-                                    
-                                )}
-                            </>
-                        ) : (
-                            <a href={`#${item}`}>{item}</a>
-                        )}
-                    </li>
-                ))}
-            </ul>
-            <div className="app__navbar-search">
-                <input type='text' placeholder='Search anything' />
-            </div>
-            <div className="app__navbar-menu">
-                <HiMenu onClick={() => setToggle(true)} />
+const menuItems = [
+    { name: 'home', link: '/' },
+    { name: 'company', link: '#' },
+    { name: 'projects', link: '/projects' }
+];
+const subMenuItems = [
+    { name: 'About Us', link: '/about' },
+    { name: 'Careers', link: '/notfound' },
+    { name: 'Blogs', link: '/notfound' },
+    { name: 'Contact Us', link: '/contact' },
+    { name: 'CSR', link: '/notfound' },
+    { name: "FAQ'S", link: '/faq' }
+];
 
-                {toggle && (
-                    <motion.div
-                        whileInView={{ x: [300, 0] }}
-                        transation={{ duration: 0.85, ease: 'easeOut' }}
-                        className='app__navbar-menu-links'
-                    >
-                        <HiX onClick={() => setToggle(false)} />
-                        <ul>
-                            {['home', 'company', 'projects'].map((item) => (
-                                <li key={item}>
-                                    {item === 'company' ? (
-                            < >
-                                <div className='flex flex-row items-center justify-start w-full' onClick={() =>{ setIsCompanyOpen(!isCompanyOpen)}}>
-                                   {item}
-                                    <IoIosArrowDown />
-                                </div>
-                                {isCompanyOpen && (
-                                    <motion.div
-                                    whileInView={{  opacity: [0,1]}}
-                                    transation={{ duration: 1.5, ease: 'easeInOut', }}
-                                    className='app__navbar-dropdown-mobile'
-                                    >
-                                        <ul >
-                                        {['About Us', 'Carrers', 'Blogs', 'Contact Us', 'CSR', "FAQ'S"].map((item, index) => (
-                                        <li key={index}>
-                                            <a href="#">{item}</a>
-                                        </li>
+return (
+    <nav className='app__navbar'>
+        <div className="app__navbar-logo">
+            <img src={'./logo.svg'} alt='logo' />
+        </div>
+        <ul className='app__navbar-links'>
+            {menuItems.map((item) => (
+                <li className='app__links p-text' key={`link-${item}`}>
+                    {item.name === 'company' ? (
+                        <>
+                            <div className='flex flex-row gap-x-2  w-full items-center justify-center app__navbar-link-icon' onClick={() => setIsCompanyOpen(!isCompanyOpen)}>
+                                {item.name}
+                                <IoIosArrowDown />
+                            </div>
+                            {isCompanyOpen && (
+                                <motion.div
+                                    whileInView={{ opacity: [0, 1] }}
+                                    transation={{ duration: 1.5, ease: 'easeInOut' }}
+                                    className='app__navbar-dropdown'
+                                >
+                                    <ul className="">
+                                        {subMenuItems.map((item, index) => (
+                                            <li key={index}>
+                                                <Link to={`${item.link}`}>{item.name}</Link>
+                                            </li>
                                         ))}
                                     </ul>
-                                    </motion.div>
-                                    
+                                </motion.div>
+                            )}
+                        </>
+                    ) : (
+                        <Link to={`${item.link}`}>{item.name}</Link>
+                    )}
+                </li>
+            ))}
+        </ul>
+        <div className="app__navbar-search">
+            <input type='text' placeholder='Search anything' />
+        </div>
+        <div className="app__navbar-menu">
+            <HiMenu onClick={() => setToggle(!toggle)} />
+
+            {toggle && (
+                <motion.div
+                    whileInView={{ x: [-300, 0] }}
+                    transation={{ duration: 1, ease: 'easeOut' }}
+                    className='app__navbar-menu-links'
+                >
+                    <HiX onClick={() => setToggle(!toggle)} />
+                    <ul>
+                        {menuItems.map((item, index) => (
+                            <li key={index}>
+                                {item.name === 'company' ? (
+                                    <>
+                                        <div className='flex flex-row items-center justify-start w-full' onClick={() => { setIsCompanyOpen(!isCompanyOpen) }}>
+                                            {item.name}
+                                            <IoIosArrowDown />
+                                        </div>
+                                        {isCompanyOpen && (
+                                            <motion.div
+                                                whileInView={{ opacity: [0, 1] }}
+                                                transation={{ duration: 1.5, ease: 'easeInOut' }}
+                                                className='app__navbar-dropdown-mobile'
+                                            >
+                                                <ul>
+                                                    {subMenuItems.map((item, index) => (
+                                                        <li key={index}>
+                                                            <Link to={`${item.link}`}>{item.name}</Link>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </motion.div>
+                                        )}
+                                    </>
+                                ) : (
+                                    <Link to={`${item}`} onClick={() => setToggle(!toggle)}>{item.name}</Link>
                                 )}
-                            </>
-                        ) : (
-                            <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
-                        )}
-                                    {/* <a href={`#${item}`} >
-                                        {item}
-                                    </a> */}
-                                </li>
-                            ))}
-                        </ul>
-                    </motion.div>
-                )}
-            </div>
-        </nav>
+                            </li>
+                        ))}
+                    </ul>
+                </motion.div>
+            )}
+        </div>
+    </nav>
 )
 };
 
